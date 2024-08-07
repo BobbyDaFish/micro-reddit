182f1f2204cc783847229892764e4d12c57e546b
BobbyDaFish <130507595+BobbyDaFish@users.noreply.github.com>
2024-08-07 14:43:00 -0600

    Add foreign key validation to post and comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   validates :title, length: { in: 8..24 }, presence: true
   validates :description, length: { maximum: 1200 }
+  validates :user_id, presence: true
 
   belongs_to :user
   has_many :comments
